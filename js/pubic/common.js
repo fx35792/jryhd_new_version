055f37cfde6e5cd87a11798425f8e2892c1bedbc
fx35792 <1873005958@qq.com>
2016-03-07 10:44:17 +0800

    index 完善

--- js/pubic/common.js
+++ js/pubic/common.js
@@ -19,4 +19,15 @@ $(function(){
 		$(this).children('.dropbox').hide();
 	});
 
+	//导航跟着屏幕滚动效果
+	$(window).scroll(function() {
+		var scroll_target = $(this).scrollTop();
+		if (scroll_target>=495) {
+        	$("#navigation").addClass("navigation");
+		}else{
+			$("#navigation").removeClass("navigation");
+		}
+	});
+
+
 });
