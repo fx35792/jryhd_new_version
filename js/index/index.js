567602181124b4108bf063bedf3aa53f60296385
fx35792 <1873005958@qq.com>
2016-03-09 18:36:52 +0800

    add

---
+++ js/index/index.js
@@ -0,0 +1,29 @@
+$(function(){
+	//商场购物 倒计时
+	$(".fnTimeCountDown").fnTimeCountDown();
+
+	//股东介绍图片切换
+	var lilength = $(".shareholder_content ul li").length+1;
+	$(".ul_width").css("width",lilength*144);
+	var leftpos = 0;
+	var leftcount = 0;
+	//左侧点击切换
+	$("#mover_left").click(function() {			
+		if (leftcount >=1)
+		{
+			leftpos = leftpos + 144;
+			leftcount = leftcount - 1;
+			$(".ul_width").animate({ left: leftpos }, "slow");				
+		}
+	});
+	//右侧点击切换
+	$("#mover_right").click(function() {
+		if (leftcount <= 0) {
+			leftpos = leftpos - 144;
+			leftcount = leftcount + 1;
+			$(".ul_width").animate({ left: leftpos }, "slow");
+		}				
+	});
+
+	
+});
