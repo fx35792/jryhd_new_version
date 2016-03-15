704eedaaef2bef2508bd9e80f55416584846fa47
fx35792 <1873005958@qq.com>
2016-03-15 15:12:02 +0800

    completed weath.html

---
+++ js/wealth/wealth.js
@@ -0,0 +1,13 @@
+$(function(){
+	$(".join").click(function(){
+		$(".mask").show();
+		$(".content").show();
+		$(".img1").addClass('animated bounceInDown').show(500);
+		 $('body').css("overflow","hidden");
+	});
+	$(".img2").click(function(){
+		$(".content").hide();
+		$(".mask").hide();
+		$('body').css("overflow-y","scroll");
+	});
+})
