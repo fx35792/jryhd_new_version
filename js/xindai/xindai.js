415c8bea4850c0becf8fa9e495282796c8a5aa95
fx35792 <1873005958@qq.com>
2016-03-22 16:26:54 +0800

    信贷页面完成

--- js/xindai/xindai.js
+++ js/xindai/xindai.js
@@ -1,20 +1,3 @@
 $(function(){
-	//房产受理条件添加鼠标划入效果
-	$(".h_bg").hover(function() {
-		$(this).find("h2").addClass("animated fadeInDown");
-		$(this).find("p").addClass("animated fadeInUp");		
-	}, function() {
-		$(this).find("h2").removeClass("animated fadeInDown");
-		$(this).find("p").removeClass("animated fadeInUp");		
-	});   
-	//业务流程添加鼠标划入效果
-	$(".yw_ico").hover(function() {
-		$(this).find("h3").addClass("animated bounceInDown");
-		$(this).find("h2").addClass("animated fadeInLeft");
-		$(this).find("p").addClass("animated fadeInRight");		
-	}, function() {
-		$(this).find("h3").removeClass("animated bounceInDown");
-		$(this).find("h2").removeClass("animated fadeInLeft");
-		$(this).find("p").removeClass("animated fadeInRight");		
-	});   
+
 });
